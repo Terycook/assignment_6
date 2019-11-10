7a0abbdc7bed085d30d31eddda01114615a648e9
Tery Cook <cook48@nsuok.edu>
2019-11-09 21:38:35 -0600

    Initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EnemyController : MonoBehaviour
+{
+    public NavMeshAgent enemyAgent;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        enemyAgent.updateRotation = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyAgent.SetDestination(player.transform.position);
+    }
+
+    
+}
